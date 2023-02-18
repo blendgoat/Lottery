963664b83d60927ad63105cd1a918aa0439f2922
blendgoat <noidel@yahoo.com>
2023-02-18 13:23:14 +0100

    yjhgf

--- components/Home/Mainpage.js
+++ components/Home/Mainpage.js
@@ -18,14 +18,14 @@ const style = {
   topContent: `flex flex-col lg:flex-row items-center justify-center`,
 
   detailsContainer: `w-[330px] md:w-[400px] lg:w-[500] text-xs text-[#5271ff] lg:text-base ml-4 p-4 lg:p-8 overflow-scroll   m-8 card rounded-lg h-48 shadow-xl items-center bg-indigo-900 `,
-  nftImgContainer: `w-[330px] md:w-[400px] lg:w-[500] text-[#5271ff] text-xs lg:text-base p-8 m-8 card shadow-xl bg-indigo-900 border-black rounded-lg h-48 items-center `,
+  nftImgContainer: `w-[330px] md:w-[400px] lg:w-[500] text-[#5271ff] text-xs lg:text-base p-4 lg:p-8 m-8 card shadow-xl bg-indigo-900 border-black rounded-lg h-48 items-center `,
   timeContainer: `container justify-center w-full lg:w-full bg-indigo-900 shadow-xl rounded-lg mt-8 md:mt-16 lg:mt-32 xl:mt-32  items-center p-4 h-64 flex`,
   ethcontainer: ` max-w-[280px] flex-1  mr-2 items-center justify-center `,
   ethLogo: `items-center justify-center w-[150px]  lg:w-[300px]`,
   ethPotBalance: `items-center text-4xl lg:text-9xl text-white/75  mr-32 font-bold`,
-  button: ` flex shadow-xl mb-16 xl:mb-24 w-4/5 bg:white hover:bg-gradient-to-r from-indigo-500   via-pink-500 to-pink-500  mt-16 md:mt-24 lg:mt-8 xl:mt-24 p-[0.8rem]  items-center justify-center h-16  rounded-lg cursor-pointer text-black`,
+  button: ` flex shadow-xl mb-24 xl:mb-24 w-4/5 bg:white hover:bg-gradient-to-r from-indigo-500   via-pink-500 to-pink-500  mt-16 md:mt-24 lg:mt-8 xl:mt-24 p-[0.8rem]  items-center justify-center h-16  rounded-lg cursor-pointer text-black`,
   buttonText: ` text-xl font-semibold p-3 text-slate-200 hover:text-slate-400   `,
-  searchBar: `relative flex flex-1 my-[0.8rem] w-max-[520px] h-6  items-center bg-lime-500 rounded-[0.8rem] `,
+  searchBar: `relative flex flex-1 my-[0.8rem] w-max-[520px] h-6 mt-4  items-center bg-lime-500 rounded-[0.8rem] `,
   searchBarClosed: `relative flex flex-1 my-[0.8rem] w-max-[520px] h-6  items-center bg-red-500 rounded-[0.8rem] `,
 };
 
